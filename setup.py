1504a4323440bccc98766ddcf0c19fc818f04749
Josy <josy@citium.be>
2025-02-28 11:46:57 +0100

    second try for 3.9

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     license="LGPL",
     platforms='any',
     keywords="HDL ASIC FPGA hardware design",
-    python_requires='>=3.10',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
