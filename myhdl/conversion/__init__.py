a27d61ca3fd424484cdd2f7d4c5e0643df2d01a0
Josy <josy@citium.be>
2025-04-03 11:07:18 +0200

    re-arranging __all__ import structure

--- myhdl/conversion/__init__.py
+++ myhdl/conversion/__init__.py
@@ -1,11 +1,11 @@
 from ._verify import verify, analyze, registerSimulator
 from ._converter import Converter
-from .cosimulation._cosimulation import getcosimkwargs, setupcosimobject
+from myhdl.conversion import cosimulation
 
 __all__ = ["verify",
            "analyze",
            "registerSimulator",
            "Converter",
-           "getcosimkwargs",
-           "setupcosimobject",
+           # "getcosimkwargs",
+           # "setupcosimobject",
            ]
---
+++ myhdl/conversion/cosimulation/__init__.py
@@ -0,0 +1,5 @@
+from ._cosimulation import getcosimkwargs, setupcosimobject
+
+__all__ = ["getcosimkwargs",
+           "setupcosimobject",
+           ]
