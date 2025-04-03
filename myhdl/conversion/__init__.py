bc3f3328ee01629a371adc792f3631924207072f
Josy <josy@citium.be>
2025-04-03 10:50:00 +0200

    adding cosimulation supprt functions to import tree

--- myhdl/conversion/__init__.py
+++ myhdl/conversion/__init__.py
@@ -1,8 +1,11 @@
 from ._verify import verify, analyze, registerSimulator
 from ._converter import Converter
+from .cosimulation._cosimulation import getcosimkwargs, setupcosimobject
 
 __all__ = ["verify",
            "analyze",
            "registerSimulator",
            "Converter",
+           "getcosimkwargs",
+           "setupcosimobject",
            ]
